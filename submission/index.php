fe6b8366938b5fbc205782b25fa0537c2471f604
Quthbuddin Pajriannor <50710300+SQuthbu@users.noreply.github.com>
2019-12-10 12:12:27 +0700

    update css form

--- submission/index.php
+++ submission/index.php
@@ -1,7 +1,7 @@
 <html>
  <head>
  <Title>Registration Form</Title>
- <style type="text/css">
+<!--  <style type="text/css">
  	body { background-color: #fff; border-top: solid 10px #000;
  	    color: #333; font-size: .85em; margin: 20; padding: 20;
  	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
@@ -13,17 +13,30 @@
  	table { margin-top: 0.75em; }
  	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
  	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
- </style>
+ </style> -->
+<style>
+    <?php include 'index.css'; ?>
+</style>
+  
  </head>
  <body>
  <h1>Register here!</h1>
  <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br></br>
-       Email <input type="text" name="email" id="email"/></br></br>
-       Job <input type="text" name="job" id="job"/></br></br>
-       <input type="submit" name="submit" value="Submit" />
-       <input type="submit" name="load_data" value="Load Data" />
+ <form method="post" action="index.php" enctype="multipart/form-data" class="form">
+        <div class="form__group">
+            <input type="text" placeholder="Name" class="form__input" name="name" id="name"/>
+        </div>
+        <div class="form__group">
+            <input type="text" placeholder="Email" class="form__input"name="email" id="email"/>
+        </div>
+        <div class="form__group">
+            <input type="text" placeholder="Job" class="form__input" name="job" id="job"/>
+        </div>
+        
+
+       <input type="button" name="submit" value="Submit"  class="btn"/>      
+       <input type="submit" class="btn" name="load_data" value="Load Data" />
+      
  </form>
  <?php
     $host = "sqpappserver.database.windows.net";
